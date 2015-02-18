82702def858c4bc2ee506ad46cf5746fe79a4bc8
Jeremy Feltracco <jpfeltracco@gatech.edu>
2015-02-17 20:38:46 -0500

    Integrated almost all subscale flight code into main flight code. Makes several structure changes as well.

--- systems/rocket/FlightSoftware/FlightSoftware.ino
+++ systems/rocket/FlightSoftware/FlightSoftware.ino
@@ -1,6 +1,14 @@
-
-
-/ gt-usli-2015 flight software ver 0.0
+/* gt-usli-2015 flight software ver 0.3 */
+/*
+ * About:
+ * Flight code for the teeensy in the rocket body.
+ * There is a function for every state that is 
+ * called in a loop with a speed based on loop_time_ms.
+ * Update methods update global variables that are 
+ *     relied upon in different parts of the code.
+ * Log methods log data that is updated using the 
+ *     update methods.
+ */
 #include <SPI.h>
 #include <Wire.h>
 #include <Servo.h>
@@ -24,7 +32,7 @@ template <typename T_ty> const char * TypeInfo<T_ty>::name = "unknown";
 MAKE_TYPE_INFO( int )
 MAKE_TYPE_INFO( float )
 MAKE_TYPE_INFO( short )
-//100ms
+
 enum states
 {
     INIT,             /* Initalize avionics, error check and report */
@@ -32,7 +40,7 @@ enum states
     LAUNCH_RDY,       /* Armed on launchpad */
     LIFTOFF,          /* Powered flight until clear of launch rail */
     PWR_ASC,          /* Powered flight */
-    DESCENT          /* Descent with drogue deployed */
+    DESCENT           /* Descent with drogue deployed */
 };
 
 /* Boot into INIT state */
@@ -52,18 +60,14 @@ const int PWR_ASC_SPEED = MILLIS / 20; // Loop at 20 Hz
 
 /* Variables */
 int i;
-int loop_time_ms = MILLIS / 20; // Filler value, gets overwritten asap
+int loop_time_ms = 0;
 unsigned long init_time;
 unsigned long init_launch_time;
-int currAltitude, prev_altitude;
+int curr_altitude, prev_altitude;
 
 sensors_event_t accelEvent;
 unsigned long lastLedBlink = 0;
 bool ledOn = 0;
-
-
-
-boolean active_control = false;
 File file;
 
 void setup()
@@ -94,9 +98,14 @@ void loop()
 
     long delay_time = loop_time_ms - (millis() - init_time);
     if (delay_time > 0) delay(delay_time);
-    else Serial.println("Trying to loop too quickly.");
 }
 
+/*
+ * Initialize accelerometer, SD card and servo motors.
+ * Report errors to serial
+ * (Altimeter doesn't need to be initialized, may need
+ * to put in error check for it)
+ */
 void state_init()
 {
     Serial.begin(9600);
@@ -108,7 +117,6 @@ void state_init()
     } else {
         Serial.println("ADXL345 initialized.");
         accel.setRange(ADXL345_RANGE_16_G);
-        active_control = true; // Only use active control if accelerometer is found
     }
 
     /* Initialize SD Card */
@@ -125,8 +133,16 @@ void state_init()
     int pins[3] = {14,15,20};
     for (i = 0; i < 3; i++)
         servos[i].attach(pins[i]);
+
+    state = LAUNCH_STDBY;
+    loop_time_ms = 0;
 }
 
+/*
+ * Standby state, blink LED and wait for arm switch
+ * to be triggered. Run as fast as possible because
+ * we are polling the arm button.
+ */
 void state_launch_stdby()
 {
     if (millis() - lastLedBlink > MILLIS / 10) // Blink at 10 Mhz
@@ -135,7 +151,7 @@ void state_launch_stdby()
         digitalWrite(ledPin, ledOn);
     }
     /* STATE CHANGE */
-    if (true) // If arm_button_pressed();
+    if (true) // If arm_button_pressed
     {
         digitalWrite(ledPin, HIGH);
         loop_time_ms = LAUNCH_RDY_SPEED; // Loop at max speed (1000 Mhz)
@@ -143,82 +159,108 @@ void state_launch_stdby()
     }
 }
 
+/*
+ * Loop as fast as we can get accelerometer readings.
+ * When we detect an upwards acceleration (maybe filter
+ * a bit?) go into liftoff state.
+ */
 void state_launch_rdy()
 {
     /* Update accel data only, don't worry about logging */
     accel.getEvent(&accelEvent);
 
     /* STATE CHANGE */
-    if (accelEvent.acceleration.y > 3) {
+    if (abs(accelEvent.acceleration.y) > 18) {
         loop_time_ms = PWR_ASC_SPEED;
         init_launch_time = millis();
         state = PWR_ASC;
     }
 }
 
+/*
+ * Update and log until we clear the launch rail
+ * based on amount of time since launch started.
+ */
 void state_liftoff()
 {
-    updateAndLog();
+    updateLogAll();
 
     /* STATE CHANGE */
-    if (millis() - init_launch_time > LAUNCH_RAIL_TIME_MS) state = PWR_ASC;
+    if (millis() - init_launch_time > LAUNCH_RAIL_TIME_MS) {
+        /* Extend servos to nominal position */
+        setServos(35);
+        state = PWR_ASC;
+    }
 }
 
+/*
+ * Active control state, update and log, then based
+ * on that information, calculate our new servo 
+ * positions. Exit when a current altitude is
+ * less than a previous altitude (reached apogee).
+ */
 void state_pwr_asc()
 {
-    updateAndLog();
+    updateLogAll();
 
-    if (active_control)
-    {
-        // Still work to be done here!
-        updateAltitude();
-        int ndx = init_launch_time / loop_time_ms;
-        int ideal_h = table_h[ndx];
-        int ideal_v = table_v[ndx];
-    }
+    int ndx = init_launch_time / loop_time_ms;
+    if (ndx > 500) ndx = 500;
+    int ideal_h = table_h[ndx];
+
+    int epsilon = curr_altitude - ideal_h;
+    setServos(getTheta(epsilon));
     
     /* STATE CHANGE */
     //return true if last altitude > current altitude (ie we are past apogee)
-    int alt = 0; // This is temporary!
-    if (prev_altitude > alt) state = DESCENT;
+    if (prev_altitude > curr_altitude) {
+        state = DESCENT;
+        setServos(0);
+    }
 }
 
+/*
+ * Update and log sensor values only.
+ */
 void state_descent()
 {
-    updateAndLog();
+    updateLogAll();
 }
 
 /* Subroutines */
 
-/*
- * Get a servo theta value given an altitude and 
- * velocity.
- */
-int lookup(double altitude, double deltaVel)
-{
-    // Check if in range first
-    // return lookup[altitude][deltaVel];
-    return 0;
-}
-
 void setServos(int theta)
 {
     // 0->160 , 110->50
-    int pos = 160-theta;
-    for(i = 0; i < 3; i++)
-      servos[i].write(pos);
+    if (theta > 90)
+        theta = 90;
+    else if (theta < 0)
+        theta = 0;
+    int pos = 160 - theta;
+    for (i = 0; i < 3; i++)
+        servos[i].write(pos);
 }
 
 /*
- * Updates sensor data and logs to SD card.
+ * Update accelerometer and altimeter and log all information.
  */
-void updateAndLog()
+void updateLogAll()
 {
     accel.getEvent(&accelEvent);
-    char buf[100]; // Is 100 enough?
-    sprintf(buf, "Time: %ul, ALT: %d, ACCEL(x,y,z), %f, %f, %f\n", 
-        millis() - init_launch_time, currAltitude, accelEvent.acceleration.x, 
-        accelEvent.acceleration.y, accelEvent.acceleration.z);
+    updateAltitude();
+    logAll();
+}
+
+/*
+ * Log information from time, accelerometer event, and whatever
+ * is in the currAltitude variable.
+ */
+void logAll()
+{
+    char buf[100];
+    sprintf(buf, "TIME: %ul, ACCEL(x,y,z), %f, %f, %f, ALT: %f", 
+        millis() - init_launch_time, accelEvent.acceleration.x, 
+        accelEvent.acceleration.y, accelEvent.acceleration.z, 
+        curr_altitude);
 }
 
 /* 
@@ -236,26 +278,43 @@ boolean logSD(char* str)
 }
 
 /*
- * Returns an integer altitude when called.
+ * Updates previousAltitude and currAltitude to
+ * reflect the latest info from the altimeter.
  * TODO: Test how quickly this can be called!
  */
-int updateAltitude()
+void updateAltitude()
 {
     if (Serial1.available())
     {
         static char input[16];
         static uint8_t i;
         char c = Serial1.read();
-        if (c!- '\r' && i < 15) // CR is chosen in serial monitor as line end
+        if (c != '\r' && i < 15) // CR is chosen in serial monitor as line end
             input[i++] = c;
         else
         {
             input[i] = '\0';
             i = 0;
             int alt_ft = atoi(input);
-            alt_m = alt_ft * .304;
+            int alt_m = alt_ft * .304;
+            prev_altitude = curr_altitude;
+            curr_altitude = alt_m;
         }
     }
-    previousAltitude = currAltitude;
-    currAltitude = alt_m
+}
+
+/*
+ * Returns a theta value for the servos given a
+ * difference between current altitude and ideal
+ * altitude.
+ */
+int getTheta (double eps) {
+    int theta;
+    if (abs(eps) <= 0.5)
+        theta = 35;
+    else if (eps >= 0)
+        theta = 90;
+    else if (eps < 0)
+        theta = 0;
+    return theta;
 }
---
+++ systems/rocket/FlightSoftware/tables.h
@@ -0,0 +1,2 @@
+// To be filled out with matlab
+int table_h[501];
