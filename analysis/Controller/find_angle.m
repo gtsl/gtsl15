592574f3ebd96b855533952f5144e23689164813
Jack <jack.ridderhof@gmail.com>
2015-03-26 23:52:07 -0400

    started on find_area function, still need to implement the iteration

--- analysis/Controller/find_angle.m
+++ analysis/Controller/find_angle.m
@@ -3,5 +3,5 @@ function angle = find_angle(area)
 % maps area of pins to commanded angle
 % If area is too large, default to max angle
 
-
+angle = 0;
 end
--- analysis/Controller/generate_table.m
+++ analysis/Controller/generate_table.m
@@ -1,22 +1,21 @@
-dt = 0.01;
-tvec = 0:dt:20;
-nSteps = length(tvec);
-hmax = 3000;
-hdotmax = 500;
-table = zeros(hmax + 100, hdotmax + 100)
-% Nominal path is size (2, nSteps). Table should include +- 100 for h and
-% hdot. Flight software will default to switch controller when outside of 
-% range.
-% 
-%
-%
+% Generate lookup table for (h, hdot) --> (Servo angle command)
 
+% Max altitude and vertical velocity expected
+hmax = 3000; % (ft)
+hdotmax = 500; % (ft/s)
+% Allocate table to store all possible h, hdot combinations
+table = zeros(hmax + 100, hdotmax + 100);
+% Rocket parameters
+Cd_r = .5; % --- changeme ----
+A_r = .1; % (m2)
+
+% Populate table
 for i = 1:size(table, 1)
     for j = 1:size(table, 2)
         % Use spacing of 1 so values = own index
         % i = h
         % j = hdot
-        area = find_area(i, j);
+        area = find_area(i, j, Cd_r, A_r);
         table(i, j) = find_angle(area);
     end
 end
